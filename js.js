f8c5e8567bb1e4eec036c0b47babbc0167fa4966
Nilufer <n.sevde@hotmail.com>
2023-07-03 21:12:36 +0300

    add style

--- js.js
+++ js.js
@@ -7,6 +7,7 @@ const humidity = document.querySelector(".humidity")
 const windd = document.querySelector(".wind");
 const degree = document.querySelector(".deg");
 const toggleButton = document.querySelector(".toggle");
+const gif = document.querySelector('.gifimg');
 
 //fetching the data when submit the location 
 searchForm.addEventListener('submit', function(event) {
@@ -15,6 +16,8 @@ searchForm.addEventListener('submit', function(event) {
     fetchWeatherData(searchValue);
 });
   
+let currentData = null; // Variable to store current weather data
+
 
 async function fetchWeatherData(location) {
     try {
@@ -30,11 +33,12 @@ async function fetchWeatherData(location) {
       }
   
       const data = await response.json();
-      console.log(data);
       // Process the retrieved weather data
+      
       const weatherData = processData(data);
-      console.log(weatherData);
+      currentData = weatherData;
       UI(weatherData.city, weatherData.country, weatherData.temperature, weatherData.feelsLike, weatherData.condition, weatherData.humidity, weatherData.wind )
+      getSticker (weatherData.condition)
     } 
 
     catch (error) {
@@ -49,40 +53,59 @@ function processData(data) {
   const weatherData = {
     city: location.name,
     country: location.country,
-    temperature: current.temp_c,
     condition: current.condition.text,
     feelsLike: current.feelslike_c,
     wind: current.wind_mph,
     humidity: current.humidity,
+    c:current.temp_c,
+    f:current.temp_f,
   };
+  console.log(weatherData)
+  console.log(weatherData.country);
+  if (toggleButton.innerHTML === "Display °F") {
+    weatherData.temperature = current.temp_c;
+  } else {
+    weatherData.temperature = current.temp_f;
+  }
   return weatherData;
 }
 
 const UI = (city, country, temp, feels, cond, humid, wind) => {
-    cityCountryName.textContent = city +"," +country;
+    cityCountryName.textContent = city + ", " + country;
     weatherCondition.textContent = cond;
     temperature.textContent = temp;
     feelsLike.textContent = "Feels like: " + feels + "°";
-    humidity.textContent = "Humidty: " + humid + "%";
-    windd.textContent = "Wind:" + wind +"mp";
+    humidity.textContent = "Humidity: " + humid + "%";
+    windd.textContent = "Wind: " + wind +"mp";
   };
 
 
 
-fetchWeatherData('Istanbul');//when first open the page 
+fetchWeatherData('amsterdam');//when first open the page 
 
-const convertCelsiusToFahrenheit = (celsius) => (celsius * 9) / 5 + 32;
-const convertFahrenheitToCelsius = (fahrenheit) => ((fahrenheit - 32) * 5) / 9;
+
 toggleButton.addEventListener("click", () => {
     if (toggleButton.innerHTML === "Display °F") {
         toggleButton.innerHTML = "Display °C";
         degree.innerHTML = "°F"
-        temperature.textContent =  convertCelsiusToFahrenheit(temperature.textContent);
+        temperature.textContent = currentData.f ;
 
     } else {
-        console.log(toggleButton.innerHTML);
+        
         toggleButton.innerHTML = "Display °F";
         degree.innerHTML = "°C"
-        temperature.textContent =  convertFahrenheitToCelsius(temperature.textContent);
+        temperature.textContent =  currentData.c;
+        console.log(currentData.f)
             }
     })
+
+  
+ async function getSticker (condition) {
+      try {
+        const response = await fetch(`https://api.giphy.com/v1/stickers/translate?api_key=9uqTcpTT1MVaSfTDL9ZPT6ML69LFAfKn&s=${condition}`, {mode: "cors"})
+        const sticker = await response.json();
+        gif.src = sticker.data.images.original.url;
+      } catch (error){
+        console.log(error);
+      }
+};
